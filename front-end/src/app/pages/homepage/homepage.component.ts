f33a816f1b19bbfb2be9feae0215afdb046365c4
Nguyen Dai <ngtrdai290197@gmail.com>
2019-09-28 21:17:43 +0700

    (feat): update with redis backend

--- front-end/src/app/pages/homepage/homepage.component.ts
+++ front-end/src/app/pages/homepage/homepage.component.ts
@@ -1,24 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { CategoryService } from 'src/@core/services/category.service';
-import { ICategory, IProduct } from 'src/@core/interface';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
-import { ProductService } from 'src/@core/services/product.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+import { CategoryService } from "src/@core/services/category.service";
+import { ICategory, IProduct } from "src/@core/interface";
+import { NgxSpinnerService } from "ngx-spinner";
+import { SwiperConfigInterface } from "ngx-swiper-wrapper";
+import { ProductService } from "src/@core/services/product.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'shop-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  selector: "shop-homepage",
+  templateUrl: "./homepage.component.html",
+  styleUrls: ["./homepage.component.scss"]
 })
 export class HomepageComponent implements OnInit {
-
   categorys: ICategory[] = [];
   products: IProduct[] = [];
   config: SwiperConfigInterface = {
     a11y: true,
-    direction: 'horizontal',
+    direction: "horizontal",
     slidesPerView: 1,
     keyboard: true,
     mousewheel: false,
@@ -29,7 +28,7 @@ export class HomepageComponent implements OnInit {
   currentPage = 1;
   pagination = {
     page: 1,
-    perPage: 16
+    perPage: 24
   };
   page: any;
 
@@ -39,7 +38,7 @@ export class HomepageComponent implements OnInit {
     private spinner: NgxSpinnerService,
     private productService: ProductService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.spinner.show();
@@ -57,11 +56,13 @@ export class HomepageComponent implements OnInit {
         total: response.total
       };
       this.spinner.hide();
-    })
+    });
   }
 
-  onSearch(keyword: string){
-    this.router.navigate(['/page-search'], { queryParams: { keyword: this.slug(keyword) } });
+  onSearch(keyword: string) {
+    this.router.navigate(["/page-search"], {
+      queryParams: { keyword: this.slug(keyword) }
+    });
   }
 
   getCategories() {
@@ -79,27 +80,28 @@ export class HomepageComponent implements OnInit {
 
   nextAndPrevious(action: string) {
     this.spinner.show();
-    this.pagination.page = action == 'pre' ? --this.currentPage : ++this.currentPage;
+    this.pagination.page =
+      action == "pre" ? --this.currentPage : ++this.currentPage;
     this.getProducts();
   }
 
   private slug(name: string) {
     name = name.toLowerCase();
-    name = name.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
-    name = name.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
-    name = name.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
-    name = name.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
-    name = name.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
-    name = name.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
-    name = name.replace(/(đ)/g, 'd');
-    name = name.replace(/([^0-9a-z-\s])/g, '');
-    name = name.replace(/(\s+)/g, '-');
-    name = name.replace(/^-+/g, '');
-    name = name.replace(/-+$/g, '');
+    name = name.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, "a");
+    name = name.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, "e");
+    name = name.replace(/(ì|í|ị|ỉ|ĩ)/g, "i");
+    name = name.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, "o");
+    name = name.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, "u");
+    name = name.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, "y");
+    name = name.replace(/(đ)/g, "d");
+    name = name.replace(/([^0-9a-z-\s])/g, "");
+    name = name.replace(/(\s+)/g, "-");
+    name = name.replace(/^-+/g, "");
+    name = name.replace(/-+$/g, "");
     return name;
   }
 
   onSetTitle() {
-    this.title.setTitle('Trang chủ');
+    this.title.setTitle("Trang chủ");
   }
 }
--- front-end/src/app/pages/page-search/page-search.component.ts
+++ front-end/src/app/pages/page-search/page-search.component.ts
@@ -1,74 +1,80 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { SearchService } from 'src/@core/services/search.service';
-import { IProduct } from 'src/@core/interface';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { SearchService } from "src/@core/services/search.service";
+import { IProduct } from "src/@core/interface";
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
-  selector: 'shop-page-search',
-  templateUrl: './page-search.component.html',
-  styleUrls: ['./page-search.component.scss']
+  selector: "shop-page-search",
+  templateUrl: "./page-search.component.html",
+  styleUrls: ["./page-search.component.scss"]
 })
 export class PageSearchComponent implements OnInit {
-
   products: IProduct[] = [];
   page: any;
   isLoading = false;
-  keyword = '';
-  selected = '1';
-  selectedSex = '0'
+  keyword = "";
+  selected = "1";
+  selectedSex = "0";
   currentPage = 1;
   pagination = {
     page: 1,
-    perPage: 10
-  }
+    perPage: 20
+  };
   constructor(
     private activatedRoute: ActivatedRoute,
     private searchService: SearchService,
     private spinner: NgxSpinnerService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe(params => {
       this.spinner.show();
-      this.keyword = params['keyword'];
+      this.keyword = params["keyword"];
       this.onChangeAndFilter();
     });
   }
 
   onLoading(action: string) {
-    if(action == 'reload'){
+    if (action == "reload") {
       this.pagination.page = 1;
     }
-    this.keyword = this.keyword === 'show' ? '' : this.keyword;
-    this.searchService.search(this.keyword, parseInt(this.selected), +this.selectedSex, this.pagination).subscribe(data => {
-      this.products = data.products;
-      this.page = {
-        current: data.current,
-        pages: new Array(data.pages),
-        total: data.total
-      };
-      this.isLoading = true;
-      this.spinner.hide();
-    });
+    this.keyword = this.keyword === "show" ? "" : this.keyword;
+    this.searchService
+      .search(
+        this.keyword,
+        parseInt(this.selected),
+        +this.selectedSex,
+        this.pagination
+      )
+      .subscribe(data => {
+        this.products = data.products;
+        this.page = {
+          current: data.current,
+          pages: new Array(data.pages),
+          total: data.total
+        };
+        this.isLoading = true;
+        this.spinner.hide();
+      });
   }
 
   navigatePage(pageNumber) {
     this.currentPage = +pageNumber + 1;
     this.spinner.show();
     this.pagination.page = +pageNumber + 1;
-    this.onLoading('navigate');
+    this.onLoading("navigate");
   }
 
   nextAndPrevious(action: string) {
     this.spinner.show();
-    this.pagination.page = action == 'pre' ? --this.currentPage : ++this.currentPage;
-    this.onLoading('navigate');
+    this.pagination.page =
+      action == "pre" ? --this.currentPage : ++this.currentPage;
+    this.onLoading("navigate");
   }
 
   onChangeAndFilter() {
     this.spinner.show();
-    this.onLoading('reload');
+    this.onLoading("reload");
   }
-
 }
